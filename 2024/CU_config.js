5b24dfdac6faf7013b3813790c5d81e9edb7ccfc
E_Chill <35980944+Eaz3@users.noreply.github.com>
2024-03-23 07:31:45 -0400

    Update CU_config.js

--- 2024/CU_config.js
+++ 2024/CU_config.js
@@ -15,7 +15,7 @@ var config_data = `
 			"name": "Event",
 			"code": "e",
 			"type": "event",
-			"defaultValue": "2024midtr",
+			"defaultValue": "2024mitry",
 			"required": "true",
 			"disabled": "true"
 		},
